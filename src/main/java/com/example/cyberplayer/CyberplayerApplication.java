3879e47225c8359ef9e85eb000481d35214f7904
heshibo <heshibo01@baidu.com>
2020-04-10 17:17:24 +0800

    initial

---
+++ src/main/java/com/example/cyberplayer/CyberplayerApplication.java
@@ -0,0 +1,13 @@
+package com.example.cyberplayer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CyberplayerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CyberplayerApplication.class, args);
+	}
+
+}
